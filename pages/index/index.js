baad47f186e388739ca3c84a08984f63c145690f
flasco <flasco@163.com>
2018-07-28 11:38:37 +0800

    提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -47,16 +47,18 @@ Page({
     })
   },
   clickJmp: function (e) {
-    wx.navigateTo({
-      url: `../read/read?index=0`,
-    });
-    let index = e.currentTarget.dataset.index;
-    let list = [...this.data.listx];
-    list[index].isUpdate = false;
-    list[index].latestRead = new Date().getTime();
-    insertionSort(list);
-    this.app.list = list;// 在onshow 的时候会自动同步
-    this.currentItem = this.app.list[0];
+    if(this.data.listx != null){
+      wx.navigateTo({
+        url: `../read/read?index=0`,
+      });
+      let index = e.currentTarget.dataset.index;
+      let list = [...this.data.listx];
+      list[index].isUpdate = false;
+      list[index].latestRead = new Date().getTime();
+      insertionSort(list);
+      this.app.list = list;// 在onshow 的时候会自动同步
+      this.currentItem = this.app.list[0];
+    }
   },
   onShow: function (e) {
     if (this.app == null) return;
